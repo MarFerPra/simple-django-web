29e4e1a169f77bc2acc2dbfa568e15bb2502800c
MarFerPra <marco.fp94@gmail.com>
2017-01-24 22:36:19 +0100

    Adding requirements.

--- practica_dai/settings.py
+++ practica_dai/settings.py
@@ -14,6 +14,18 @@ import os
 from pymongo import MongoClient
 DB_CLIENT = MongoClient('localhost:27017')
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
